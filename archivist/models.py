cd7ca19ac7f713b5a92481acfa89853105fdd333
thrashbaidon <jabama94@gmail.com>
2022-05-06 11:53:27 -0500

    apuntes requerimientos

--- archivist/models.py
+++ archivist/models.py
@@ -2,5 +2,25 @@ from django.db import models
 
 # Create your models here.
 '''
-documentos necesarios
+documentos necesarios:
+1 comprobante de PTC
+2 comprobante de nombramiento de postgrado en UPE
+3.a comprobante de Docencia una asignatura curricular (4 horas a la semana) durante los ciclos 2021, 2020
+y 2019
+
+3.b comrpobante de Generación o aplicación innovadora del conocimiento/investigación
+    aplicada, asimilación, desarrollo y transferencia de
+    tecnología/innovación, investigación aplicada y desarrollo tecnológico
+
+        Productos válidos que serán considerados:
+        ➢ Libros y Capítulos de libro
+        ➢ Artículos arbitrados y artículos indexados
+        ➢ Artículos arbitrados y artículos indexados:
+        ➢ Modelos de utilidad.
+        ➢ Patentes.
+        ➢ Prototipos.
+        ➢ Transferencia de tecnología.
+        ➢ Desarrollo de infraestructura
+        ➢ Informes Técnicos
+        ➢ Obras artísticas:
 '''
